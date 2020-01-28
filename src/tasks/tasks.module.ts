224cd8fe7d2ef0c500fd8b074d6629c282eb67a2
Liran Mery <liran@panorays.com>
2020-01-28 08:59:00 +0200

    Section 5: (Part 1) Authentication - Setting up JWT/Passport.js

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,12 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepsitory } from './task.repository';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TaskRepsitory]),
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
